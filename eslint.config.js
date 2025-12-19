7f39b2785f97bb470f6f8ad1f460ce7a9fd04cd3
Saju G <saju.g@wontonetech.com>
2025-12-19 16:14:49 +0530

    updations

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,9 @@ export default defineConfigWithVueTs(
         rules: {
             'vue/multi-word-component-names': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
-            '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/ban-ts-comment': 'off',
+            'vue/no-v-text-v-html-on-component': 'off',
         },
     },
     prettier,
