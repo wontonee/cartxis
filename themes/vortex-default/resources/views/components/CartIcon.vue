6401672dafb9d47f724f9e1a634f75767d7f800a
Saju G <saju.g@wontonetech.com>
2025-12-19 16:46:18 +0530

    updation

--- themes/vortex-default/resources/views/components/CartIcon.vue
+++ themes/vortex-default/resources/views/components/CartIcon.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { computed, onMounted } from 'vue';
 import { Link } from '@inertiajs/vue3';
-import { useCart } from '@/Composables/useCart';
+import { useCart } from '@/composables/useCart';
 
 const { itemCount, fetchCart } = useCart();
 
--- themes/vortex-default/resources/views/components/ProductCard.vue
+++ themes/vortex-default/resources/views/components/ProductCard.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue';
 import { Link, router } from '@inertiajs/vue3';
-import { useCart } from '@/Composables/useCart';
+import { useCart } from '@/composables/useCart';
 import { useCurrency } from '@/composables/useCurrency';
 
 const { formatPrice } = useCurrency();
